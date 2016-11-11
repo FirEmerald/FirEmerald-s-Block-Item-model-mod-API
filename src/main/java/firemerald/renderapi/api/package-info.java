c50b30ec69f757d2af6adc1b6fe9ad004d62b33d
FirEmerald <mccaslinwalt@gmail.com>
2016-11-10 18:02:59 -0600

    stuff that didn't go for some reason

--- src/main/java/firemerald/renderapi/api/package-info.java
+++ src/main/java/firemerald/renderapi/api/package-info.java
@@ -2,6 +2,7 @@
  * (C) 2016 FirEmerald
  * please note this API is subject to change.
  */
+@API(apiVersion = "1.0", owner = "FirEmerald", provides = "FirEmerald's Block/Item model API")
 package firemerald.renderapi.api;
 
 import net.minecraftforge.fml.common.API;
